be8ac6570c21f845d76ba4beb3be5c8636c2c268
Dae Heum Na <daeheumna@gmail.com>
2023-03-16 18:00:36 -0400

    added icons to skills section & styled contact form in indext HTML

--- script.js
+++ script.js
@@ -41,3 +41,11 @@ var typingEffect = new Typed('.autoType', {
 
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
