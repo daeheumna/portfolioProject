8257988940bf61c10c7ca630ef5dc652e3850236
Dae Heum Na <daeheumna@gmail.com>
2023-03-19 19:48:48 -0400

    added a fade-in animation as user scrolls to project section

--- script.js
+++ script.js
@@ -69,3 +69,22 @@ modals.forEach(function (trigger) {
     });
   });
 });
+
+
+
+
+const observer = new IntersectionObserver((enteries) => {
+    enteries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } 
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
